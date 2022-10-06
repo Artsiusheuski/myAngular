084b18805377e907a7bdb4a12aaa383808990d0f
Artsiusheuski <i.artsiusheuski@gmail.com>
2022-10-06 13:18:17 +0200

    5

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http'; // также нужно добавить в modulle import
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http'; // также нужно добавить в modulle import
 import { IProduct } from '../models/product';
-import { Observable } from 'rxjs';
+import { catchError, delay, Observable, throwError } from 'rxjs';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root', // for auto regist.
 })
 export class ProductsService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private errorService: ErrorService) {} // for reqest
 
   getAll(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
-      params: new HttpParams({
-        fromObject: { limit: 8 }, //second arg.- params for request
-      }),
-    });
+    return this.http
+      .get<IProduct[]>('https://fakestoreapi.com/products1', {
+        // params: new HttpParams().append('limit', 3),
+        params: new HttpParams({
+          fromObject: { limit: 5 },
+        }),
+      })
+      .pipe(
+        // from rxjs, to slow down loading(для замедления загрузки)
+        delay(2000),
+        catchError(this.errorMSG.bind(this))
+      );
+  }
+
+  private errorMSG(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
+    return throwError(() => error.message);
   }
 }
